e0e33e51ff285aecc2d5b0d66e1774eca59ba386
Alex Darigan <47497501+AlexDarigan@users.noreply.github.com>
2024-12-29 17:10:45 +0000

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,11 @@
-print("hello world")
+from appwrite.client import Client
+
+client = Client()
+client.set_endpoint('https://cloud.appwrite.io/v1') # Your API Endpoint
+client.set_project('67717c4e003447c84281') # Your project ID
+client.set_key('<YOUR_API_KEY>') # Your secret API key
+
+functions = Functions(client)
+print("Hello World")
+
+return {"greeting": "hello"}
